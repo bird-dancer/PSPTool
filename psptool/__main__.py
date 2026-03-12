78d6133c64008e7614824262ba5377a1e9fe7198
Felix Dumbeck <felix@dumbeck.net>
2026-03-12 19:29:27 +0100

    repair

--- psptool/__main__.py
+++ psptool/__main__.py
@@ -139,8 +139,6 @@ def main():
         parser.print_help(sys.stderr)
         sys.exit(0)
 
-    sys.exit("asdf")
-
     psp = PSPTool.from_file(args.file, verbose=args.verbose)
     output = None
 
